96a7f6de1cbf7b60f47072e18814ecdfe427e09f
Tretyak Andru <andruuu5482@gmail.com>
2021-05-18 13:02:27 +0300

    ss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div className="main-menu">
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </div>
+
+        </nav>
+    );
+}
+export default Navbar;
